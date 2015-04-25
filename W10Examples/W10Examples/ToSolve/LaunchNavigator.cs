a8a1de6ab3245f54950a55c1489167bc84acb0d6
JuanPablo <juanpablo@expediteapps.com>
2015-04-25 22:44:52 +0100

    launcher.launchuriasync

---
+++ W10Examples/W10Examples/ToSolve/LaunchNavigator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace W10Examples.ToSolve
+{
+    public class LaunchNavigator
+    {
+        /// <summary>
+        /// Issues
+        /// 1.- How to set PreferredApplicationDisplayName to Firefox for instance
+        /// 2.- Requires both PreferredApplicationDisplayName &  PreferredApplicationPackageFamilyName
+        /// 3.- Open Files
+        /// 4.- Open Explorer Folders
+        /// </summary>
+        /// <param name="uri"></param>
+        public async void TestLauncher(string uri)
+        {
+            var options = new LauncherOptions();
+            options.DisplayApplicationPicker = false;
+            options.PreferredApplicationDisplayName = "Mozilla Firefox";
+            var success = await Launcher.LaunchUriAsync(new Uri(uri, UriKind.Absolute), options);
+        }
+    }
+}
